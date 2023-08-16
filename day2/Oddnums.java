495e45bb3ace27241ba0dc0db79be5fca3d3ec58
pree2302 <60914952+pree2302@users.noreply.github.com>
2023-08-16 12:18:32 +0530

    Add files via upload
    Working on "missing element in an array". I have completed the other prgms.

---
+++ day2/Oddnums.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+import java.util.Iterator;
+
+public class Oddnums {
+	
+	
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0; i<=20; i++)
+		{
+			if(i%2!=0)
+			{
+				
+				System.out.println("Number is odd  " +i);
+			}
+		}
+
+	
+	}
+
+}
