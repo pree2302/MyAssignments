21341e04d41ece0dc61d229bc43b920e40608319
pree2302 <60914952+pree2302@users.noreply.github.com>
2023-08-29 12:05:11 +0530

    Week3.Day2

---
+++ assignmnts/SortUsingCollection.java
@@ -0,0 +1,25 @@
+package week3.day2.assignmnts;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SortUsingCollection {
+
+	public static void main(String[] args) {
+		
+		String[] str = {"HCL", "Wipro", "Aspire Systems", "CTS"};
+        int length = str.length;
+        
+        List<String> list1 = Arrays.asList(str); 
+        Collections.sort(list1); 
+        
+        for (int i = length - 1; i >= 0; i--) 
+        { 
+            System.out.print(list1.get(i));
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+}
+}
