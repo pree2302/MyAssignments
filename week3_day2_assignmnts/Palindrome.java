ef3311fbef5121db8db5b03faa72c4b784841913
pree2302 <60914952+pree2302@users.noreply.github.com>
2023-08-29 13:25:07 +0530

    Add files via upload

---
+++ week3_day2_assignmnts/Palindrome.java
@@ -0,0 +1,27 @@
+package week3_day2_assignmnts;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		
+		String str="Madam";
+		str=str.toLowerCase();
+		
+		
+		String rev="";
+		
+		for (int i = str.length() - 1; i >= 0; i--) 
+		{
+            rev = rev + str.charAt(i);
+        }
+		
+		if(str.equals(rev))
+		{
+			System.out.println(str+ " Is a Palindrome String");
+		}
+		else
+			System.out.println(str+ " Not A Palindrome String");
+
+	}
+
+}
---
+++ week3_day2_assignmnts/PrintDuplicatesInArray.java
@@ -0,0 +1,25 @@
+package week3_day2_assignmnts;
+
+public class PrintDuplicatesInArray {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {14,12,13,11,15,14,18,16,17,19,18,17,20};
+		
+		System.out.println("Duplicate Values are:");
+		for (int i=0; i < arr.length; i++)
+		{
+			for (int j = i+1; j < arr.length; j++)
+			{
+				if (arr[i]==arr[j])
+				{
+					
+					System.out.println(arr[i]);
+				}
+			}
+		}
+		
+
+	}
+
+}
